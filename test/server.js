9f01274295be340723e6924311a9b04258b8b6f3
johnhof <john.hofrichter@gmail.com>
2016-10-24 10:48:36 -0700

    implemented middleware

--- test/server.js
+++ test/server.js
@@ -11,30 +11,130 @@ let zeromatter = require('../lib');
 let Zeromatter = require('../lib/zeromatter');
 
 describe('server', () => {
-  it('should initialize a server instance', function *() {
-    let app = zeromatter();
-    expect(app).to.be.instanceof(Zeromatter);
-    expect(app.config).to.be.an('object');
-    expect(app.middleware).to.be.an('array');
-    expect(app.socket).to.not.be.undefined;
-    expect(app.socket.identity).to.be.a('string');
+  describe('initialization', () => {
+    it('should initialize a server instance', function *() {
+      let app = zeromatter();
+      expect(app).to.be.instanceof(Zeromatter);
+      expect(app.config).to.be.an('object');
+      expect(app.middleware).to.be.an('array');
+      expect(app.socket).to.not.be.undefined;
+      expect(app.socket.identity).to.be.a('string');
+    });
+
+    it('should apply configuration when initialized', function *() {
+      let override = { host: 'localhost', port: 1991 };
+      let app = zeromatter(override);
+      expect(app.config.protocol).to.equal(DEFAULTS.config.protocol);
+      expect(app.config.username).to.equal(DEFAULTS.config.username);
+      expect(app.config.password).to.equal(DEFAULTS.config.password);
+      expect(app.config.host).to.equal(override.host);
+      expect(app.config.port).to.equal(override.port);
+    });
+
+    it('should listen on a default host/port and respond to all string messages', function *() {
+      let app = zeromatter();
+      app.listen();
+      let res = yield zquest({ data: 'TEST' });
+      expect(res).to.be.a('string');
+      app.close();
+    });
+
+    it('should listen on a default host/port and respond to all json messages', function *() {
+      let app = zeromatter();
+      app.listen();
+      let res = yield zquest({ data: { text: 'TEST' } });
+      expect(res).to.be.a('string');
+      app.close();
+    });
   });
 
-  it('should apply configuration when initialized', function *() {
-    let override = { host: 'localhost', port: 1991 };
-    let app = zeromatter(override);
-    expect(app.config.protocol).to.equal(DEFAULTS.config.protocol);
-    expect(app.config.username).to.equal(DEFAULTS.config.username);
-    expect(app.config.password).to.equal(DEFAULTS.config.password);
-    expect(app.config.host).to.equal(override.host);
-    expect(app.config.port).to.equal(override.port);
-  });
+  describe('middleware', () => {
+    describe('.use', () => {
+      it('should allow addition of a generator', function *() {
+        let expected = 'test';
+        let i = 0;
+        let o = 0;
+        let app = zeromatter();
+        app.use(function *(next) { i++; yield next(); o++; });
+        app.use(function *() { i++; this.response = expected; o++; });
+        app.listen();
+        let res = yield zquest({ data: 'TEST' });
+        expect(i).to.equal(2);
+        expect(o).to.equal(2);
+        expect(res).to.equal(expected);
+        app.close();
+      })
 
-  it('should listen on a default host/port and respond to all messages', function *() {
-    let app = zeromatter();
-    yield app.listen();
-    console.log('request')
-    let res = yield zquest({ data: 'TEST' });
-    console.log(res);
+      it('should allow addition of a promise', function *() {
+        let expected = 'test';
+        let i = 0;
+        let o = 0;
+        let app = zeromatter();
+        app.use(function (next) {
+          return new Promise((resolve, reject) => {
+            i++; next().then(() => {
+              o++; resolve();
+            });
+          });
+        });
+        app.use(function () {
+          return new Promise((resolve, reject) => {
+            i++; this.response = expected; o++; resolve();
+          });
+        });
+        app.listen();
+        let res = yield zquest({ data: 'TEST' });
+        expect(i).to.equal(2);
+        expect(o).to.equal(2);
+        expect(res).to.equal(expected);
+        app.close();
+      });
+    });
+
+    describe('.useAll', () => {
+      it('should allow addition of a generator', function *() {
+        let expected = 'test';
+        let i = 0;
+        let o = 0;
+        let app = zeromatter();
+        app.useAll([
+          function *(next) { i++; yield next(); o++; },
+          function *() { i++; this.response = expected; o++; }
+        ]);
+        app.listen();
+        let res = yield zquest({ data: 'TEST' });
+        expect(i).to.equal(2);
+        expect(o).to.equal(2);
+        expect(res).to.equal(expected);
+        app.close();
+      })
+
+      it('should allow addition of a promise', function *() {
+        let expected = 'test';
+        let i = 0;
+        let o = 0;
+        let app = zeromatter();
+        app.useAll([
+          function (next) {
+            return new Promise((resolve, reject) => {
+              i++; next().then(() => {
+                o++; resolve();
+              });
+            });
+          },
+          function () {
+            return new Promise((resolve, reject) => {
+              i++; this.response = expected; o++; resolve();
+            });
+          }
+        ]);
+        app.listen();
+        let res = yield zquest({ data: 'TEST' });
+        expect(i).to.equal(2);
+        expect(o).to.equal(2);
+        expect(res).to.equal(expected);
+        app.close();
+      })
+    });
   });
 });
